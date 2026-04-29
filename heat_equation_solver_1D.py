ff97e51c4d1333fa7550b46a3ef81e0a1a35976f
Devin <244196806qq@gmail.com>
2026-04-28 17:52:12 -0700

    Change to calculate once at a time

--- heat_equation_solver_1D.py
+++ heat_equation_solver_1D.py
@@ -2,30 +2,45 @@ import numpy as np
 
 
 
-# solver
-def solve_heat_1D(initial_temp, r, numTimes):
-    temps = [
-        np.array(initial_temp, dtype = float) # store initial as np array
-        ]
-    # simulate for n times
-    for _ in range(numTimes):
-        current_state = temps[-1] # the current temperature values 
-        u_new = current_state.copy()
+# # batch solver
+# def solve_heat_1D(initial_temp, r, numTimes):
+#     temps = [
+#         np.array(initial_temp, dtype = float) # store initial as np array
+#         ]
+#     # simulate for n times
+#     for _ in range(numTimes):
+#         current_state = temps[-1] # the current temperature values 
+#         u_new = current_state.copy()
 
-        # update the temperature at each position
-        u_new[1:-1] = current_state[1:-1] + r * (
-            current_state[2:] - 2 * current_state[1:-1] + current_state[:-2]
-        ) 
+#         # update the temperature at each position
+#         u_new[1:-1] = current_state[1:-1] + r * (
+#             current_state[2:] - 2 * current_state[1:-1] + current_state[:-2]
+#         ) 
 
-        # keep boundaries the same temperature
-        u_new[0] = current_state[0]
-        u_new[-1] = current_state[-1]
+#         # keep boundaries the same temperature
+#         u_new[0] = current_state[0]
+#         u_new[-1] = current_state[-1]
 
-        # Add the new set of temperature into temps
-        temps.append(u_new)
+#         # Add the new set of temperature into temps
+#         temps.append(u_new)
 
-    return temps
+#     return temps
 
+# step by step solver
+def solve_heat_1D(current_temp, r, boundary):
+    u_new = current_temp.copy()
+    # update the temperature at each position
+    u_new[1:-1] = current_temp[1:-1] + r * (
+        current_temp[2:] - 2 * current_temp[1:-1] + current_temp[:-2]
+    ) 
+    # keep boundaries the same temperature
+    if boundary == "Dirichlet":
+        u_new[0] = current_temp[0]
+        u_new[-1] = current_temp[-1]
+    elif boundary == "Neumann":
+        u_new[0] = u_new[1]
+        u_new[-1] = u_new[-2]
+    return u_new
 
 
 # initial condition function generator 
